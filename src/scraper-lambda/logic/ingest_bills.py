2639c8c1b51c9caaeb2ba74b76e00f9e0d8f0bb4
Aksel Bell <adb117@duke.edu>
2025-11-16 18:53:39 -0500

    fix bug

--- src/scraper-lambda/logic/ingest_bills.py
+++ src/scraper-lambda/logic/ingest_bills.py
@@ -34,7 +34,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import logging
 from bs4 import BeautifulSoup
-from doc_sanitizer import sanitize_document
+from logic.doc_sanitizer import sanitize_document
 
 # Configure logging
 logging.basicConfig(
